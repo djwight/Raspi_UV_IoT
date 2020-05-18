41c2817dc4e52fedb9ad2b0fe96f2fcb49920d04
djwight <d.j.wight@gmail.com>
2020-05-18 20:05:00 +0200

    typo fixed in scripts

--- src/RPiButtonShutdown.py
+++ src/RPiButtonShutdown.py
@@ -1,5 +1,5 @@
 import os
-from gpiozero import button
+from gpiozero import Button
 
 def shutdown():
     print('Button pressed- system shutting down...')
@@ -9,7 +9,7 @@ def print_test():
     print('Button pressed, system shutting down')
 
 # Set up GPIO interactions
-btn = button(18)
+btn = Button(18)
 
 # Action code for when the button ir pressed
 btn.when_pressed = print_test
--- src/RPiShutdown.py
+++ src/RPiShutdown.py
@@ -1,7 +1,8 @@
 import os
-from gpiozero import button
+from gpiozero import Button
 
 def shutdown():
+    print('system shutting down...')
     os.system('sudo shutdown -h now')
 
 if __name__ == '__main__':
