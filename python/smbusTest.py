ed6bfa7936534b22e358a939990704ad50f76748
djwight <d.j.wight@gmail.com>
2020-05-23 23:43:27 +0200

    multiplex test

--- python/smbusTest.py
+++ python/smbusTest.py
@@ -12,5 +12,5 @@ while True:
     bus.write_byte(adress, A0)
     value = bus.read_byte(address)
     vol = value*3.3/255
-    print(f"A0:     {round(vol, 2}")
+    print(f"A0:     {round(vol, 2)}")
     time.sleep(1)
