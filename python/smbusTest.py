d88ba602d35da19bd80415937d5fcbbc58bf6927
djwight <d.j.wight@gmail.com>
2020-05-23 23:57:58 +0200

    multiplex test

--- python/smbusTest.py
+++ python/smbusTest.py
@@ -7,10 +7,15 @@ A1 = 0x41
 A2 = 0x42
 A3 = 0x43
 
-bus = smbus.SMBus(1)
+bus_1 = smbus.SMBus(1)
+bus_2 = smbus
 while True:
     bus.write_byte(address, A0)
-    value = bus.read_byte(address)
-    vol = value*3.3/255
-    print(f"A0:     {round(vol, 2)}")
+    therm_value = bus.read_byte(address)
+    therm_vol = therm_value*3.3/255
+    bus.write_byte(address, A1)
+    photo_value = bus.read_byte(address)
+    photo_vol = photo_value*3.3/255
+    print(f"A0:     {round(therm_vol, 2)}")
+    print(f"A1:     {round(photo_vol, 2)}")
     time.sleep(1)
