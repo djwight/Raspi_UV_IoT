c5502293fa82c89e1539bc9f22e6294721397ec0
djwight <d.j.wight@gmail.com>
2020-06-04 21:29:33 +0200

    exp code added

--- python/v1.2/oled_demo_experiment.py
+++ python/v1.2/oled_demo_experiment.py
@@ -18,7 +18,7 @@ def changing_var(device):
             draw.text((2, 30), f"UVA {str(i)}", font=nf, fill=1)
             draw.text((2, 45), f"UVB {str(i)}", font=nf, fill=1)
             draw.text((55, 20), f"Temp {str(i)}", font=nf, fill=1)
-            draw.text((55, 40), f"Humidity {str(i)}", font=nf, fill=1)
+            draw.text((55, 35), f"Humidity {str(i)}", font=nf, fill=1)
             draw.text((55, 50), f"Pressure {str(i)}", font=nf, fill=1)
             time.sleep(0.01)
 
