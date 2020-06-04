920cb4d1c006b91ee15a4d67ef640924a8386cbc
djwight <d.j.wight@gmail.com>
2020-06-04 21:18:26 +0200

    exp code added

--- python/v1.2/oled_demo_experiment.py
+++ python/v1.2/oled_demo_experiment.py
@@ -9,12 +9,12 @@ font_path = 'ChiKareGo.ttf'
 
 
 def changing_var(device):
-    size = 18
+    size = 12
     nf = ImageFont.truetype(font_path, size)
     datef = ImageFont.truetype(font_path, 14)
     for i in range(100):
         with canvas(device) as draw:
-            draw.text((70, 10), f"Date", font=nf, fill=1)
+            draw.text((50, 10), f"Date", font=nf, fill=1)
             draw.text((5, 25), f"UVA {str(i)}", font=nf, fill=1)
             draw.text((70, 25), f"UVB {str(i)}", font=nf, fill=1)
             draw.text((5, 45), f"Temp {str(i)}", font=nf, fill=1)
