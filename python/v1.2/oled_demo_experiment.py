d53dbf53b9988eb2c9ca9cc17e7d67356884f598
djwight <d.j.wight@gmail.com>
2020-06-04 20:34:42 +0200

    exp script with oled

---
+++ python/v1.2/oled_demo_experiment.py
@@ -0,0 +1,51 @@
+import time
+import sys
+from luma.core.interface.serial import i2c
+from luma.oled.device import sh1106
+from luma.core.render import canvas
+from PIL import ImageFont
+
+font_path = 'ChiKareGo.ttf'
+
+
+def changing_var(device):
+    size = 20
+    nf = ImageFont.truetype(font_path, size)  # new font
+    for i in range(100):
+        with canvas(device) as draw:
+            draw.text((50, 0), str(i), font=nf, fill=1)
+            time.sleep(0.005)
+
+
+def primitives(device):
+    with canvas(device) as draw:
+        # Draw an X.
+        draw.line((4, 48, 126, 62), fill=1)
+        draw.line((4, 62, 126, 48), fill=1)
+
+        # Write two lines of text.
+        draw.text((45, 20), 'AZ-Delivery', fill=1)
+
+
+try:
+    serial = i2c(port=1, address=0x3c)
+    device = sh1106(serial, rotate=0, width=128, height=64)
+    print('[Press CTRL + C to end the script!]')
+    while(True):
+        print('Testing printing variable.')
+        changing_var(device)
+        time.sleep(2)
+        print('Testing basic graphics.')
+        primitives(device)
+        time.sleep(3)
+        print('Testing display ON/OFF.')
+        for _ in range(5):
+            time.sleep(0.5)
+            device.hide()
+            time.sleep(0.5)
+            device.show()
+            print('Testing clearing display.\n')
+            device.clear()
+            time.sleep(2)
+except KeyboardInterrupt:
+    print('Script end!')
