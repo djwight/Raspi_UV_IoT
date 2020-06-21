c721762dc87b636a43edb87f98c1e1592257f24a
djwight <d.j.wight@gmail.com>
2020-06-21 20:52:08 +0200

    verified sensor works

--- python/GY-521_test/gy521_test.py
+++ python/GY-521_test/gy521_test.py
@@ -3,7 +3,17 @@ from mpu6050 import mpu6050
 
 
 if __name__ == "__main__":
-    mpu = mpu6050(0x68)
-    accel_data = sensor.get_accel_data()
-    print(accel_data)
-    time.sleep(1)
+    while True:
+        mpu = mpu6050(0x68)
+        accel_data = mpu.get_accel_data()
+        gyro_data = mpu.get_gyro_data()
+        temp_data = mpu.get_temp()
+        accel, gyro, temp = mpu.get_all_data()
+        print("")
+        print("Accel data....", accel_data)
+        print("")
+        print("gyro data...", gyro_data)
+        print(f"temp is {temp_data} oC")
+        print("")
+        print("all data", accel, gyro, temp)
+        time.sleep(2)
